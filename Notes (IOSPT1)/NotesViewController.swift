5e385961200cb53fa37093b8dab71185e947f8b8
Jason <jason@Kristens-MBP.lan>
2019-04-29 19:29:03 -0700

    Set up initial view controller and storyboards

---
+++ Notes (IOSPT1)/NotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotesViewController.swift
+//  Notes (IOSPT1)
+//
+//  Created by Jason on 4/29/19.
+//  Copyright © 2019 Jason Thomas. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+ 
+
+}
